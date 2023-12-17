89d0dbd0d666d389e771cffe426df9b6e60581d4
Web Developer <ts.analista@gmail.com>
2023-12-17 19:02:52 -0300

    feat: add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { Header } from "./components/Header";
+import { Post } from "./Post";
 
-import styles from './App.module.css';
-import './styles/global.scss';
-
+import styles from "./App.module.css";
+import "./styles/global.scss";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
-    return (
+  return (
     <div>
       <Header />
-     <div className={styles.wrapper}>
-      aside+main
-     </div>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Developer"
+            content="Ex et quis officia culpa nulla nisi commodo sit adipisicing culpa officia sit occaecat laboris."
+          />
+
+          <Post
+            author="Developer"
+            content="Ex et quis officia culpa nulla nisi commodo sit adipisicing culpa officia sit occaecat laboris."
+          />
+        </main>
+      </div>
     </div>
   );
 }
